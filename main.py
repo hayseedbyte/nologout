8058e0df5b599875ee17427ba41191bfdd8528ec
Clinton Thomas <29966176+hayseedbyte@users.noreply.github.com>
2022-08-31 12:49:13 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ import usb_hid
 from lib.adafruit_hid.keyboard import Keyboard
 from lib.adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from lib.adafruit_hid.keycode import Keycode
+import board
+import digitalio
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+x = 0
+while x < 1:
+    led.value = True
+    time.sleep(0.5)
+    led.value = False
+    time.sleep(0.5)
+    led.value = True
+    time.sleep(0.5)
+    led.value = False
+    x = 1
 
 time.sleep(1)
 keyboard = Keyboard(usb_hid.devices)
@@ -24,15 +40,15 @@ def do_every(period, f, key):
 
 
 def sim_keys(key):
+    led.value = True
     kbd.press(key)
-    # time.sleep(0.3)
     kbd.release(key)
-    print("pressed1")
+    led.value = False
     time.sleep(0.8)
+    led.value = True
     kbd.press(key)
-    # time.sleep(0.3)
     kbd.release(key)
-    print("pressed2")
+    led.value = False
 
 
 do_every(420, sim_keys, Keycode.SCROLL_LOCK)
